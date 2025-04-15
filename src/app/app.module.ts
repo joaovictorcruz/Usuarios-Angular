473638d0e89e06455c87dcc68c2247f05a25c8d8
joaovictorcruz <elietejvc@hotmail.com>
2025-04-15 10:21:35 -0300

    iniciando projeto angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CadastroComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration(withEventReplay())
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
